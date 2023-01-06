4d38bd392cf332045a79b09ab0142264b53440b7
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-06 01:27:34 -0600

    add delete endpoint

--- app.js
+++ app.js
@@ -76,6 +76,22 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   })
 });
 
+// DELETE
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}!`);
 });
