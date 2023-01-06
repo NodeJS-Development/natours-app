5dd7fa138dde41d04c7578b38a862f856f0ca19b
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-06 01:19:43 -0600

    add endpoint to fetch specific tour and capture request params

--- app.js
+++ app.js
@@ -19,6 +19,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = +req.params.id;
+  const tour = tours.find(tour => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 // POST
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
