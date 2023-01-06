51d6bf9854adc7a0cf97e94168ae7209ad9d91bc
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-06 02:03:33 -0600

    use morgan as a middleware on express

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   console.log('Hello from the middleware 👋');
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+
+// 2) ROUTE HANDLERS
 
 // GET
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
@@ -111,6 +118,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
+
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -121,6 +130,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) START SERVER
 
 app.listen(port, () => {
   console.log(`App running on port ${port}!`);
