49ac429d188c5457e26a3813e94ffc1132723060
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-11 22:24:07 -0600

    add filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,8 +3,25 @@ const Tour = require('../models/tourModel');
 // GET
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
